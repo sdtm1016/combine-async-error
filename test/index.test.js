e15d997820873035bcf3c9cb9dd5c4d77e7cd313
Lao-Qi <2231675321@qq.com>
2022-07-20 13:06:58 +0800

    添加新的测试案例

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 const combineAsyncError = require("../index.cjs")
 
-function requestJL() {
+function requestJL(agv) {
     return new Promise((res, rej) => {
         if (a[0]) {
-            res("200")
+            res(agv + "200")
         } else {
             rej("吉林")
         }
@@ -29,10 +29,10 @@ function requestYN() {
         }
     })
 }
-var a = [true, false, true]
 
+var a = [true, false, true]
 test("基本测试", () => {
-    combineAsyncError([requestJL, requestSD, requestYN], {}).then(res => {
+    combineAsyncError([requestJL, requestSD, requestYN]).then(res => {
         expect(res).toEqual({
             result: [
                 {
@@ -46,6 +46,66 @@ test("基本测试", () => {
                 msg: "山东",
                 funcName: "requestSD"
             }
+        })
+    })
+})
+
+test("不同参数测试", () => {
+    combineAsyncError([
+        requestJL,
+        {
+            func: requestSD,
+            args: ["火车票价格："],
+            callback: res => {
+                console.log(res)
+            }
+        },
+        requestYN
+    ]).then(res => {
+        expect(res).toEqual({
+            result: [
+                {
+                    flag: true,
+                    data: {
+                        msg: "火车票价格：200"
+                    }
+                }
+            ],
+            error: {
+                msg: "山东",
+                funcName: "requestSD"
+            }
+        })
+    })
+})
+
+test("forever 配置项测试", () => {
+    combineAsyncError([requestJL, requestSD, requestYN], {
+        forever: true
+    }).then(res => {
+        expect(res).toEqual({
+            result: [
+                {
+                    flag: true,
+                    data: {
+                        msg: "200"
+                    }
+                },
+                {
+                    flag: false,
+                    data: {
+                        msg: "山东",
+                        funcName: "requestSD"
+                    }
+                },
+                {
+                    flag: true,
+                    data: {
+                        msg: "400"
+                    }
+                }
+            ],
+            error: null
         })
     })
 })
